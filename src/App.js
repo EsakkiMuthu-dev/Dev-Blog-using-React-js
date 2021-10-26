da786d28fa32dd876569fa6aa40260e9c8ec992c
Harry Potter <esakkimuthu6842@gmail.com>
2021-10-26 15:51:02 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './index.css'
+import Navbar from './Navbar';
+import Home from './Home';
+import{BrowserRouter as Router,Route,Switch } from 'react-router-dom'
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 function App() {
+  // dyanmic values in react
+  //muultiple components
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+   <Router>
+    <Navbar/>
+     <div className="content">
+ 
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route exact path ='/create'>
+          <Create />
+        </Route>
+        <Route exact path ='/blogs/:id'>
+          <BlogDetails />
+        </Route>
+        <Route exact path ='*'>
+        <NotFound/>
+        </Route>
+      </Switch>
+      </div>
+    </Router>
+    
     </div>
   );
 }
